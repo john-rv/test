67893afd2ea7e5601bb9c44426988972b570a75a
john-rv <john-r@ya.ru>
2021-11-15 08:42:10 +0500

    авторизация в панели администрирования (убрал wait)

--- java-qa/src/test/java/LoginAdmin.java
+++ java-qa/src/test/java/LoginAdmin.java
@@ -4,17 +4,15 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginAdmin {
 
     private WebDriver driver;
-    private WebDriverWait wait;
 
     @Before
     public void start(){
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver,10 );
+
     }
 
     @Test
