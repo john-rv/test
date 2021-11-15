0c7dde73e6496977d5b4d99b016d426a165b96d1
john-rv <john-r@ya.ru>
2021-11-15 21:36:49 +0500

    добавлен префикс Test к названию классов

--- java-qa/src/test/java/TestLaunchBrowser.java
+++ java-qa/src/test/java/TestLaunchBrowser.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
-public class LaunchBrowser {
+public class TestLaunchBrowser {
     private WebDriver driver;
     private WebDriverWait wait;
 
